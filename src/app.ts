90a2d06379cc1d66ce813af19f3dfc5c2cccf3c3
FREDERICO AREND BINSFELD <frederico.binsfeld@mazzafc.tech>
2025-03-05 09:11:18 -0300

    feat: configure app to use InversifyExpressServer

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import express from "express";
 
 import awardsRoutes from "./routes/awards.routes";
 import { errorHandler } from "./middlewares/errorHandler";
+import { InversifyExpressServer } from "inversify-express-utils";
+import { container } from "./config/container";
 
-const app = express();
-app.use(express.json());
-app.use("/api", awardsRoutes);
-app.use(errorHandler);
+const server = new InversifyExpressServer(container);
+
+server.setConfig((app) => {
+  app.use(express.json());
+  app.use("/api", awardsRoutes);
+  app.use(errorHandler);
+});
+
+const app = server.build();
 
 export default app;
